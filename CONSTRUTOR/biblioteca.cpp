25bf4b03869f27769d41b87baa6eb7c93a2a4470
Baka-Noob <negocios-trabalho@outlook.com>
2025-03-23 11:19:07 -0300

    Adição da pasta CONSTRUTOR
    Foi movido os arquivos do professor para uma pasta separada

---
+++ CONSTRUTOR/biblioteca.cpp
@@ -0,0 +1,30 @@
+#include "biblioteca.hpp"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+    string t, a;
+    int y;
+    
+    livro livro1;
+
+    cout << "Entre o nome do livro: ";
+    getline(cin, t);   
+    livro1.set_titulo(t);
+
+    cout << "Entre o nome do autor: ";
+    getline(cin, a);
+    livro1.set_autor(a);
+
+    cout << "Entre o ano de publicacao: ";
+    cin >> y;
+    livro1.set_ano(y);
+    
+    
+    cout << "\nO livro " << livro1.get_titulo() << " foi escrito por " << livro1.get_autor() << " e publicado em " << livro1.get_ano() << endl;
+
+    return 0;
+}
